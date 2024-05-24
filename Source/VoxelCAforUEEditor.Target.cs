48bb403fa631a434bf9f288ebaa759a4cd701c84
lapollinaria <46728296+lapollinaria@users.noreply.github.com>
2024-05-24 04:47:40 +0300

    Update VoxelCAforUEEditor.Target.cs

--- Source/VoxelCAforUEEditor.Target.cs
+++ Source/VoxelCAforUEEditor.Target.cs
@@ -3,9 +3,9 @@
 using UnrealBuildTool;
 using System.Collections.Generic;
 
-public class VoxelCAforUE : TargetRules
+public class VoxelCAforUEEditorTarget : TargetRules
 {
-	public VoxelCAforUE ( TargetInfo Target) : base(Target)
+	public VoxelCAforUEEditorTarget ( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
 		DefaultBuildSettings = BuildSettingsVersion.V2;
