ae41285e74319f2d68c9110e1fdefea57b88b966
lapollinaria <46728296+lapollinaria@users.noreply.github.com>
2024-06-02 12:53:20 +0300

    Delete Source directory

--- Source/VoxelCAforUE/Private/Game/VoxelCAforUE.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "VoxelCAforUE.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, VoxelCAforUE, "VoxelCAforUE" );
